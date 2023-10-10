524aef3b2e36a71366c897d5d2b8fb2b9f9d83af
haido86 <thithuhai.do@gmail.com>
2023-10-10 09:58:52 +0300

    Pass data between screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_item.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -9,12 +9,22 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (context) => const NewItem(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   @override
@@ -34,19 +44,19 @@ class _GroceryListState extends State<GroceryList> {
         ],
       ),
       body: ListView.builder(
-        itemCount: groceryItems.length,
+        itemCount: _groceryItems.length,
         itemBuilder: (context, index) => ListTile(
           leading: Container(
-            color: groceryItems[index].category.color,
+            color: _groceryItems[index].category.color,
             width: 24,
             height: 24,
           ),
           title: Text(
-            groceryItems[index].name,
+            _groceryItems[index].name,
             style: Theme.of(context).textTheme.bodyLarge,
           ),
           trailing: Text(
-            groceryItems[index].quantity.toString(),
+            _groceryItems[index].quantity.toString(),
             style: Theme.of(context).textTheme.bodyLarge,
           ),
         ),
